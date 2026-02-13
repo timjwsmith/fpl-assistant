11e2973bf628bb569f396f038e4a9da6570cee2f
TJWS <tim@smiffs.com.au>
2026-02-13 16:25:38 +1000

    Updated index.ts

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,19 @@ import { setupVite, serveStatic, log } from "./vite";
 import automationScheduler from "./automation-scheduler";
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

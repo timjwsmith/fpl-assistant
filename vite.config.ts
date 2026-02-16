0870e523b4e1e4ac88e1d3a57c75cb18608b9760
TJWS <tim@smiffs.com.au>
2026-02-16 17:48:45 +1000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,9 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
+    host: '0.0.0.0',
+    port: 5000,
+    strictPort: false,
     fs: {
       strict: false,
       allow: [".."],
